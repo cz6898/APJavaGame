a6ff62a45e818db805f1746f692381ba60405f6c
Chairman Zhao <chairmanzhao42@gmail.com>
2016-02-12 10:10:36 -0500

    2_12_16

---
+++ src/Tile.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by Chairman Zhao on 2/12/2016.
+ */
+public class Tile extends JButton {
+    public Tile(){
+
+    }
+}
---
+++ src/UserPanel.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Chairman Zhao on 2/12/2016.
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+public class UserPanel extends JPanel implements ActionListener, MouseMotionListener{
+    public void actionPerformed(ActionEvent e){
+
+    }
+    public void mouseDragged(MouseEvent e){
+
+    }
+    public void mouseMoved(MouseEvent e){
+
+    }
+}
